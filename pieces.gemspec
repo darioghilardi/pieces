64ec77fb83550adb99682509f20ade3bd9831004
Dario Ghilardi <ghilardi@fidor.de>
2015-05-28 17:22:08 +0200

    Add sprockets to improve the rails compatibility.

--- pieces.gemspec
+++ pieces.gemspec
@@ -27,6 +27,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency "sinatra", "~> 1.4.6"
+  spec.add_dependency "sinatra-asset-pipeline", "~> 0.7.0"
 
   spec.add_development_dependency "bundler", "~> 1.9"
   spec.add_development_dependency "rake", "~> 10.0"
--- pieces.rb
+++ pieces.rb
@@ -1,7 +1,18 @@
 require 'sinatra/base'
+require 'sinatra/asset_pipeline'
 
 class Pieces < Sinatra::Application
+  register Sinatra::AssetPipeline
+
   set :bind, '0.0.0.0'
+  set :assets_prefix, %w(app/assets)
+
+  configure do
+    # Setup Sprockets
+    sprockets.append_path File.join(root, 'app', 'assets', 'stylesheets')
+    sprockets.append_path File.join(root, 'app', 'assets', 'javascripts')
+    sprockets.append_path File.join(root, 'app', 'assets', 'images')
+  end
 
   get '/' do
     erb :index, layout: :application_layout
