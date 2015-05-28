7bd18676ddebc52067cabbce159ebc1040ace713
Dario Ghilardi <ghilardi@fidor.de>
2015-05-28 23:26:34 +0200

    Add pry as dependency and use the configure block for options.

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,14 +1,17 @@
 require "pieces/version"
 require 'sinatra/base'
 require 'sinatra/asset_pipeline'
+require 'pry'
 
 module Pieces
   class App < Sinatra::Application
 
-    set :bind, '0.0.0.0'
-    set :assets_prefix, %w(app/assets)
-    set :engine_stylesheet, 'facelift/facelift'
-    set :engine_javascript, 'facelift/facelift'
+    configure do
+      set :bind, '0.0.0.0'
+      set :assets_prefix, %w(app/assets /app/assets)
+      set :engine_stylesheet, 'facelift/facelift'
+      set :engine_javascript, 'facelift/facelift'
+    end
 
     register Sinatra::AssetPipeline
 
